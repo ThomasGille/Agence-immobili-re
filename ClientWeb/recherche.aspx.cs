063c6caeb4f01444dd10c8752b50e63cf143f3a3
Transrian <valentin.bourdier@mail.com>
2016-03-10 19:26:43 +0100

    advanced research now work

--- ClientWeb/recherche.aspx.cs
+++ ClientWeb/recherche.aspx.cs
@@ -16,8 +16,20 @@ namespace ClientWeb
 
         protected void button_valider_Click(object sender, EventArgs e)
         {
-            Response.Redirect("display_result.aspx" + Request.Url.Query);
-
+            int a=456;
+            Response.Redirect("display_result.aspx?type_chauffage=" + Request["ctl00$ContentPlaceHolder1$type_chauffage"]
+                + "&type_transaction=" + Request["ctl00$ContentPlaceHolder1$type_transaction"]
+                + "&type_bien=" + Request["ctl00$ContentPlaceHolder1$type_bien"]
+                + "&energie_chauffage=" + Request["ctl00$ContentPlaceHolder1$energie_chauffage"]
+                + "&prix_demande=" + Request["prix_demande"]
+                + "&surface_demande=" + Request["surface_demande"]
+                + "&nombre_piece_demande=" + Request["nombre_piece_demande"]);
+            /*string query = "display_result.aspx?";
+            foreach(String key in Request.QueryString.AllKeys)
+            {
+                
+            }
+            Response.Redirect(query);*/
         }
     }
 }
